d54f381cd8007a54db6751101d8d1fbaf5c4483d
Marco Porro <mp.porro.marco@gmail.com>
2023-03-29 11:14:45 +0200

    add analyize_ascii_edgelists

--- run_ascii_dal.sh
+++ run_ascii_dal.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 counter=0
+desc=${1:-Default description}
 for pair in ascii_edgelists/* ; do
+    if [ ! -d "$pair" ]; then
+        continue
+    fi
     counter=$((counter+1))
     if [ $counter -eq 3 -o $counter -eq 5 ]; then
         continue
     fi
-    echo "Processing $pair"
     g1="$pair/g1.txt"
     g2="$pair/g2.txt"
-    #timeout=3000
-    timeout=$(cat "$pair/timeout.txt")
-    ./mcsplit-dal -A -t $timeout min_max $g1 $g2 2>&1 | tee "$pair/result-dal.txt"
+    timeout=300
+    #timeout=$(cat "$pair/timeout.txt")
+    echo "Processing $pair with timeout $timeout"
+    current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+    outfile="$pair/result-dal-$current_time.txt"
+    ./mcsplit-dal -A -t $timeout min_max $g1 $g2 2>&1 | tee $outfile
+    echo $desc >> $outfile
+    echo "timeout: $timeout" >> $outfile
 done 
 python3 /home/porro/telecho/telecho.py marco done
